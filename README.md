4cceee91d7b490e369615c0c91670e7d19c22b56
Sohil876 <sohil876@gmail.com>
2021-03-11 17:50:37 +0530

    Add switch case implementation with help function

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Build-Bot
 
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/39aee766b2e542448db3ecfecf75faf9?branch=bbot-python)](https://app.codacy.com/gh/Sohil876/Build-Bot?utm_source=github.com&utm_medium=referral&utm_content=Sohil876/Build-Bot&utm_campaign=Badge_Grade_Settings)
+
+**Instructions:**
+
+-   Give -h or --help argument to script for help and see available commands.
+
--- bbot.py
+++ bbot.py
@@ -4,3 +4,28 @@
 #-# By @Sohil876
 #-# https://github.com/Sohil876/Build-Bot
 #
+
+from sys import argv as arg, exit as sys_exit
+
+# Vars
+
+
+#Functions
+def bbot_help():
+    print('''
+  -h    Shows brief help
+  ''')
+
+# Switch case implementation
+#arg = argv
+switcher = {
+    '-h' : bbot_help,
+    }
+
+if len(arg) == 2 and arg[1] in switcher:
+    switcher[arg[1]]()
+else:
+    print('Invalid argument!')
+    print('See -h for list of arguments')
+    sys_exit(1)
+
