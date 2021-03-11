07fa08c6c93ff35b5fd63843cbbead4d31604565
Sohil876 <sohil876@gmail.com>
2021-03-11 17:52:53 +0530

    Add ascii banner

--- bbot.py
+++ bbot.py
@@ -5,6 +5,7 @@
 #-# https://github.com/Sohil876/Build-Bot
 #
 
+from pyfiglet import figlet_format
 from sys import argv as arg, exit as sys_exit
 
 # Vars
@@ -12,7 +13,9 @@ from sys import argv as arg, exit as sys_exit
 
 #Functions
 def bbot_help():
-    print('''
+    print(f'''
+{figlet_format("Build-Bot", font="letters")}
+{figlet_format("By :- Sohil876", font="digital")}
   -h    Shows brief help
   ''')
 
@@ -25,6 +28,7 @@ switcher = {
 if len(arg) == 2 and arg[1] in switcher:
     switcher[arg[1]]()
 else:
+    print(f'{figlet_format("Build-Bot", font="letters")}\n{figlet_format("By :- Sohil876", font="digital")}')
     print('Invalid argument!')
     print('See -h for list of arguments')
     sys_exit(1)
